283d197415b37bb7e20406e6c7b4dad08bdd0f83
William Farnsworth <williamcoreyf@gmail.com>
2021-05-26 10:47:01 -0400

    added questions to inquirer

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the project title?',
+      },
+      {
+        type: 'inputQuestion',
+        name: 'descriptionQuestion',
+        message: 'Description of the project?',
+      },
+      {
+        type: 'input',
+        name: 'installationQuestion',
+        message: 'installation Instructions?',
+      },
+      {
+        type: 'input',
+        name: 'usageQuestion',
+        message: 'What is the intended usage?',
+      },
+      {
+        type: 'input',
+        name: 'licenseQuestion',
+        message: 'Please provide a license for this project:',
+        // license choices
+        choices: ["MIT", "The Unlicense", "GNU GPL v3", "Apache 2.0", "None"],
+      },
+      {
+        type: 'input',
+        name: 'contributionQuestion',
+        message: 'Who are the repository contributors?',
+      },
+      {
+        type: 'input',
+        name: 'testQuestion',
+        message: 'Do you have test instructions?',
+      },
+      {
+        type: 'input',
+        name: 'emailQuestion',
+        message: 'What is the contact email?',
+      }
+    ]);
+  };
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
