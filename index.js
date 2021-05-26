3746a4d6047c543e706810f474e511c284882785
William Farnsworth <williamcoreyf@gmail.com>
2021-05-26 11:28:49 -0400

    implemented inquirer questions.

--- index.js
+++ index.js
@@ -1,60 +1,62 @@
 const inquirer = require('inquirer');
 const fs = require("fs");
 
-const questions = () => {
-    return inquirer.prompt([
-      {
-        type: 'input',
-        name: 'projectTitle',
-        message: 'What is the project title?',
-      },
-      {
-        type: 'inputQuestion',
-        name: 'descriptionQuestion',
-        message: 'Description of the project?',
-      },
-      {
-        type: 'input',
-        name: 'installationQuestion',
-        message: 'installation Instructions?',
-      },
-      {
-        type: 'input',
-        name: 'usageQuestion',
-        message: 'What is the intended usage?',
-      },
-      {
-        type: 'input',
-        name: 'licenseQuestion',
-        message: 'Please provide a license for this project:',
-        // license choices
-        choices: ["MIT", "The Unlicense", "GNU GPL v3", "Apache 2.0", "None"],
-      },
-      {
-        type: 'input',
-        name: 'contributionQuestion',
-        message: 'Who are the repository contributors?',
-      },
-      {
-        type: 'input',
-        name: 'testQuestion',
-        message: 'Do you have test instructions?',
-      },
-      {
-        type: 'input',
-        name: 'emailQuestion',
-        message: 'What is the contact email?',
-      }
-    ]);
-  };
+const questions = [
+    {
+      type: 'input',
+      name: 'projectTitle',
+      message: 'What is the project title?',
+    },
+    {
+      type: 'inputQuestion',
+      name: 'descriptionQuestion',
+      message: 'Description of the project?',
+    },
+    {
+      type: 'input',
+      name: 'installationQuestion',
+      message: 'installation Instructions?',
+    },
+    {
+      type: 'input',
+      name: 'usageQuestion',
+      message: 'What is the intended usage?',
+    },
+    {
+      type: 'input',
+      name: 'licenseQuestion',
+      message: 'Please provide a license for this project:',
+      // license choices
+      choices: ["MIT", "The Unlicense", "GNU GPL v3", "Apache 2.0", "None"],
+    },
+    {
+      type: 'input',
+      name: 'contributionQuestion',
+      message: 'Who are the repository contributors?',
+    },
+    {
+      type: 'input',
+      name: 'testQuestion',
+      message: 'Do you have test instructions?',
+    },
+    {
+      type: 'input',
+      name: 'emailQuestion',
+      message: 'What is the contact email?',
+    }   
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+  inquirer.prompt(data).then((answers) => {
 
+  });
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile("README.md", questions);
+}
 
 // Function call to initialize app
 init();
